6b8356f03fcf3e5bfbe7a9674ea8103860f86697
crispusomollo <crispusomollo@gmail.com>
2025-10-27 17:08:13 +0300

    [M01] users, roles and profiles updated

--- modules/01_users_roles_privileges/module.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Module 01 - Users, Roles & Privileges
-echo "[INFO] Executing Module 01: Users, Roles & Privileges"
-sqlplus lab_admin/lab123 @create_users.sql
-sqlplus lab_admin/lab123 @grant_privileges.sql
-sqlplus lab_admin/lab123 @test_access.sql
-echo "[INFO] Module 01 completed successfully."
